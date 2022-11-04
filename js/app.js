c35d6e37ad058b7e27a994f746dd403bdaf3246a
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-11-04 16:42:59 +0100

    [fix] task già presente

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-/***Descrizione:**
-Rifare l’esercizio della to do list.
-Ogni todo sarà un oggetto, formato da due proprietà:
-- `text`, una stringa che indica il testo del todo
-- `done`, un booleano (true/false) che indica se il todo è stato fatto oppure no
-Steps:
-1. avere un elenco di task da fare o fatti
-2. stamparli in pagina
-3. al click del task fare il toggle del task fatto o meno
-4. al click della croce eliminare il task se fatto, altrimenti scrivere un messaggio di errore
-5. dopo aver scritto il task con invio o al click del bottone aggiungere il nuovo task (come non fatto) in cima all’elenco dei task
-6. verificare la lunghezza minima del nuovo task
-7. se l’elenco è vuoto non mostrare la lista ma mostrare il messaggio “Non ci sono task”
+/*
 BONUS
 - Verificare se il task è già presente e nel caso mostrare l’errore
 - ... il limite è la fantasia ;-)
@@ -61,25 +49,17 @@ createApp({
 
         //se più lunga salvo il dato in una const
       }else{
-        const newTaskTodo = {
-          text: this.newTask,
-          done: false
-        }
 
-        //ciclo tutti gli elementi dell' array tasks
-        this.tasks.forEach(element => {
-
-          //salvo nella variabile text di tasks
-          const {text} = element;
-          console.log(text);
-
-          //se text è uguale a il dato appena inserito blocca funzione con msgError e non aggiunge la task
-          if(text === this.newTask){
-            console.log('--------->già presente');
-            return this.errorMsg = 'Attenzione! La Task è già presente!'
+        //con find confronto i task.text di tasks con newTask
+        if (this.tasks.find(task => task.text === this.newTask)) {
+          this.errorMsg ='Attenzione! La task esiste già!'
+        } else {
+          const newTaskTodo = {
+            text: this.newTask,
+            done: false
           }
-        });
-        this.tasks.unshift(newTaskTodo);
+          this.tasks.unshift(newTaskTodo);
+        }
       }
       this.newTask='';
     }
